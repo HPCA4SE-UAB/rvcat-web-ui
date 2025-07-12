d3e85114725ce67df5174f6a75a60dac799bc119
JCMoure <juancarlos.moure@uab.es>
2025-07-12 19:25:02 +0200

    RVCAT CODE

--- public/rvcat_code.js
+++ public/rvcat_code.js
@@ -21,8 +21,7 @@ function addNewProgram(config){
 }
 
 const RUN_PROGRAM_PREAMBLE = function() {
-    let res = `rvcat._scheduler.load_program(rvcat._program, iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
-    return res;
+    let res = return res;
 }
 
 const RVCAT_HEADER = function() {
@@ -33,8 +32,8 @@ const RVCAT_HEADER = function() {
       res += `rvcat._processor.load_processor('${currentProcessor()}')\n`
     }
     if (prog !== undefined) {
-      res += `rvcat._program.load_program('${currentProgram()}')\n`
-      res += RUN_PROGRAM_PREAMBLE();
+      res += `rvcat._program.load_program('${currentProgram()}')\n` ;
+      res += `rvcat._scheduler.load_program(rvcat._program, iterations=${currentIterations()}, window_size=${currentROBSize()})\n` ;
     }
     return res;
 }
