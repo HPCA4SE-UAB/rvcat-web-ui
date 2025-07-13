c8173fb1fd3c3b0ddc34483b8015c4fdae3feac2
JCMoure <juancarlos.moure@uab.es>
2025-07-13 10:40:42 +0200

    RVCAT CODE show dependencies

--- public/rvcat_code.js
+++ public/rvcat_code.js
@@ -1,3 +1,11 @@
+const PROG_SHOW                         = 'str(rvcat._program)'
+const PROG_SHOW_CRITICAL_PATHS_GRAPHVIZ = `rvcat._program.get_recurrent_paths_graphviz()`
+const PROG_SHOW_EXECUTION               = `rvcat._program.annotate_execution()`
+const PROG_SHOW_STATIC_PERFORMANCE      = `rvcat._program.show_dependencies()`
+const SHOW_PROCESSOR                    = 'rvcat._processor.json()'
+const GET_PROGRAM_JSON                  = 'rvcat._program.json()'
+const RUN_PROGRAM_ANALYSIS              = 'rvcat._scheduler.format_analysis_json()'
+
 const GET_AVAIL_PROGRAMS = `import rvcat
 rvcat._program.list_programs_json()
 `
@@ -5,24 +13,9 @@ rvcat._program.list_programs_json()
 const GET_AVAIL_PROCESSORS = `import rvcat
 rvcat._processor.list_processors_json()
 `
-
-const PROG_SHOW              = 'str(rvcat._program)'
-const PROG_SHOW_CRITICAL_PATHS_GRAPHVIZ = `rvcat._program.get_recurrent_paths_graphviz()`
-const PROG_SHOW_EXECUTION    = `rvcat._program.annotate_execution()`
-const PROG_SHOW_STATIC_PERFORMANCE = `rvcat.show_program_performance()`
-
 function show_timeline(num_iters) {
     return `rvcat._scheduler.format_timeline(niters=${num_iters})`
 }
-
-const SHOW_PROCESSOR = 'rvcat._processor.json()'
-const GET_PROGRAM_JSON = 'rvcat._program.json()'
-
-const RUN_PROGRAM_PREAMBLE = function() {
-    let res = `rvcat._scheduler.load_program(rvcat._program, iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
-    return res;
-}
-const RUN_PROGRAM_ANALYSIS = 'rvcat._scheduler.format_analysis_json()'
 
 function addModifiedProcessor(config){
   let res = `rvcat._processor.import_processor_json(${JSON.stringify(config)})`;
@@ -34,10 +27,15 @@ function addNewProgram(config){
   return res;
 }
 
+const RUN_PROGRAM_PREAMBLE = function() {
+    let res = `rvcat._scheduler.load_program(rvcat._program, iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
+    return res;
+}
+
 const RVCAT_HEADER = function() {
     let proc = currentProcessor();
     let prog = currentProgram();
-    let res = `import rvcat\n`;
+    let res  = `import rvcat\n`;
     if (proc !== undefined) {
       res += `rvcat.load_processor('${currentProcessor()}')\n`
     }
