c507f4a7b9ac4dc3782b02ac49db628831fd18a8
JCMoure <juancarlos.moure@uab.es>
2025-12-22 21:38:01 +0100

    About

--- src/components/aboutComponent.vue
+++ src/components/aboutComponent.vue
@@ -6,7 +6,7 @@
       <p>RVCAT (Code Analysis Tool) is an educational simulation tool that demonstrates how processor architecture impacts the execution performance of program loops.</p>
       <p>RVCAT has been developed on the Computer Architecture and Operating Systems (CAOS) Department of the Universitat Autònoma de Barcelona (UAB) by Saul Adserias, Quim Aguado, Bern Vich, Aleix Jorda and Juan C. Moure.</p>
       <p>The simulator is still under active development. If you find any bugs or want to help improving the tools, please contact us (JuanCarlos.Moure@uab.es).</p>
-      <p>We want to thank the support from Santiago Marco and Carmen del Toro.</p>
+      <p>We want to thank the support from Santiago Marco, Albert Jimenez and Carmen del Toro.</p>
       <p><b>The RVCAT developers © 2026</b></p>
     </div>
 </template>
