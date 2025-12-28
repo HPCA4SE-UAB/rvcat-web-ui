6b0f34b56c2bc8cf39bfb1e807ff9d859838f197
JCMoure <juancarlos.moure@uab.es>
2025-12-28 20:49:37 +0100

    Change SVG height to auto in staticAnalysisComponent

--- src/components/staticAnalysisComponent.vue
+++ src/components/staticAnalysisComponent.vue
@@ -313,12 +313,10 @@
     position: absolute;
     inset:    0;
     width:    100%;
-    height:   100%;
+    height:   auto;
     max-width:  100%;
-    max-height: 100%; 
     display:    block;
   }
-
 
   .icon-button {
     border:    none;
