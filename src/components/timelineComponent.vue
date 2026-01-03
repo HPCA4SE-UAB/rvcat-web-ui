efb33d201e4f38a00d31042f8216284f49b7768e
JCMoure <juancarlos.moure@uab.es>
2026-01-03 17:27:29 +0100

    Update timelineComponent.vue

--- src/components/timelineComponent.vue
+++ src/components/timelineComponent.vue
@@ -764,16 +764,18 @@
       </div>
     </div>
   </div>
-  
-  <TutorialComponent 
-     v-if="showTutorial" 
-     :position="tutorialPosition"
-     title="Timeline"
-     text= "<p>The <strong>Timeline</strong> section shows the program execution over time. 
-              The number of <em>loop iterations</em> can be modified, and the timeline can be <strong>zoomed in/out</strong>.</p>
-           <p>Hover over the grid to see basic info about the selected cell, and <em>click</em> to obtain more detailed information.</p>"
-     @close="closeTutorial"
-  />
+
+  <Teleport to="body">
+    <TutorialComponent 
+       v-if="showTutorial" 
+       :position="tutorialPosition"
+       title="Timeline"
+       text= "<p>The <strong>Timeline</strong> section shows the program execution over time. 
+                The number of <em>loop iterations</em> can be modified, and the timeline can be <strong>zoomed in/out</strong>.</p>
+             <p>Hover over the grid to see basic info about the selected cell, and <em>click</em> to obtain more detailed information.</p>"
+       @close="closeTutorial"
+    />
+  </Teleport>
   
   <div v-if="clickedCellInfo" class="modal-overlay" @click.self="clickedCellInfo = null">
     <div class="modal">
