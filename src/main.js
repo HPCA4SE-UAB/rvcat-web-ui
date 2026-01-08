872d29dfb7458faca90a76aff9ec3a07c34734fd
JCMoure <juancarlos.moure@uab.es>
2026-01-08 20:35:49 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import router          from './router'
 const app = createApp(App)
 
 // Create reactive state BEFORE mounting
-const robState = reactive({
+/* const robState = reactive({
   ROBsize: 10
 })
 
 // Provide it to all components
 app.provide('robState', robState)
+*/
 
 app.use(createPinia())
 app.use(router)
