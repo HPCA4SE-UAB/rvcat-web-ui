29c28293834b13292b38dd362f3a92c88e9d9590
JCMoure <juancarlos.moure@uab.es>
2026-01-08 20:20:05 +0100

    Add ROBsize reactive state

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
-import { createApp }   from 'vue'
+import { createApp, reactive }   from 'vue'
 import { createPinia } from 'pinia'
 import App             from './App.vue'
 import router          from './router'
 
 const app = createApp(App)
 
+// Create reactive state BEFORE mounting
+const robState = reactive({
+  ROBsize: 10
+})
+
+// Provide it to all components
+app.provide('robState', robState)
+
 app.use(createPinia())
 app.use(router)
-app.use({
-  install(app) {
-    const robState = Vue.reactive({
-      ROBsize: 10
-    });
-    
-    // Make available globally
-    app.config.globalProperties.$rob = robState;
-    app.provide('robState', robState);
-  }
-});
 
 app.mount('#app')
