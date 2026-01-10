1ab814a23f5f9d3bb6e11307709cd6e7f9ac001e
JCMoure <juancarlos.moure@uab.es>
2026-01-10 20:16:14 +0100

    Include Loading State

--- src/views/SimulatorView.vue
+++ src/views/SimulatorView.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { ref, shallowRef, onMounted, onUnmounted, inject, nextTick, watch } from 'vue';
 import headerComponent         from '@/components/headerComponent.vue';
-import loadingComponent        from '@/components/loadingComponent.vue';
 import processorComponent      from '@/components/processorComponent.vue';
 import programComponent        from '@/components/programComponent.vue';
 import timelineComponent       from '@/components/timelineComponent.vue';
@@ -101,7 +100,12 @@ function setLoadingOverlayMessage(message) {
       />
     </header>
 
-    <loadingComponent />
+    <div class="blur-overlay" id="blur-overlay-item"></div>
+      <div class="overlay" id="loading-overlay">
+        <div class="spinner"></div>
+        <p id="loading-overlay-message">Initializing</p>
+        <p>Please wait. Loading can take several seconds</p>
+    </div>
 
     <main class="container">
       <div class="grid-item processor">
@@ -157,4 +161,75 @@ function setLoadingOverlayMessage(message) {
 .processor { grid-column: 1; grid-row: 1; }
 .program   { grid-column: 1; grid-row: 2; }
 .results   { grid-column: 2; grid-row: 1 / 3; min-width: 0;}
+  
+.blur-overlay {
+    position: fixed;
+    display: none;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgb(65, 65, 65, 0.5);
+    z-index: 10;
+    backdrop-filter: blur(5px);
+}
+.overlay {
+    position: fixed;
+    display: none;
+    margin: auto;
+    width: fit-content;
+    height: fit-content;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(255, 255, 255, 0.4);
+    z-index: 100;
+    border-radius: 15px;
+    border: 2px solid #506c8666;
+    padding: 2%;
+    padding-right: 5%;
+    padding-left: 5%;
+}
+
+#loading-overlay p {
+    margin: auto;
+    font-weight: bold;
+    text-align: center;
+    font-size: 2.5vh;
+}
+
+/* Spinner animation */
+.spinner {
+    border: 12px solid #f3f3f3;
+    border-top: 12px solid #007bff;
+    border-radius: 50%;
+    width: 80px;
+    height: 80px;
+    animation: spin 1s linear infinite;
+    margin: auto;
+    margin-bottom: 20px;
+}
+
+.spinner-small {
+    border: 3px solid #f3f3f3;
+    border-top: 3px solid #868686;
+    border-radius: 50%;
+    width: 15px;
+    height: 15px;
+    animation: spin 1s linear infinite;
+    margin: auto;
+}
+
+@keyframes spin {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
 </style>
