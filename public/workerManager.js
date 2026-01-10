f19ab3f724ade8dff934bac8942bdd375c9a94f5
JCMoure <juancarlos.moure@uab.es>
2026-01-10 19:53:32 +0100

    Waits for Ready

--- public/workerManager.js
+++ public/workerManager.js
@@ -1,18 +1,31 @@
 // Pure JavaScript, no Vue dependencies
 export class WorkerManager {
   constructor(workerPath = './worker.js') {
-    this.worker = new Worker(workerPath);
+    this.worker           = new Worker(workerPath);
     this.messageCallbacks = new Map(); // Store callbacks by message ID
-    this.globalHandlers = {}; // Will be set by Vue components
+    this.globalHandlers   = {}; // Will be set by Vue components
+    
+    this.isReady      = false; // Track initialization state
+    this.readyPromise = null; // Promise for waiting
+    this.readyResolve = null;
     
     this.setupMessageHandler();
+    this.initialize();
   }
 
   setupMessageHandler() {
     this.worker.onmessage = (message) => {
-      const { action, id, result, data_type } = message.data;
-      
+      const { action, id, result, data_type } = message.data;     
       console.log('Worker message:', message.data);
+      
+      if (action === 'initialized') {
+        this.isReady = true;
+        if (this.readyResolve) {
+          this.readyResolve();
+          this.readyResolve = null;
+        }
+        console.log('Worker initialized and ready');
+      }
       
       if (action === 'executed' && id) {
         // Call specific callback if exists
@@ -21,16 +34,10 @@ export class WorkerManager {
           callback(result, data_type);
           this.messageCallbacks.delete(id); // Clean up
         }
-        
-        // Call global handler if exists
+
         if (this.globalHandlers[id]) {
           this.globalHandlers[id](result, data_type);
         }
-      }
-      
-      if (action === 'initialized') {
-        // Worker is ready
-        this.initialized = true;
       }
     };
   }
@@ -45,17 +52,23 @@ export class WorkerManager {
     delete this.globalHandlers[id];
   }
 
-  // Execute code with callback
-  execute(code, id, callback) {
-    if (callback) {
-      this.messageCallbacks.set(id, callback);
+  // Wait for initialization
+  async waitForReady() {
+    if (this.isReady) return true;
+    
+    if (!this.readyPromise) {
+      this.readyPromise = new Promise((resolve) => {
+        this.readyResolve = resolve;
+      });
     }
     
-    this.worker.postMessage({
-      action: 'execute',
-      code: code,
-      id: id
-    });
+    return this.readyPromise;
+  }
+
+  // Execute only when ready
+  async executeWhenReady(code, id, callback) {
+    await this.waitForReady();
+    return this.execute(code, id, callback);
   }
 
   initialize() {
@@ -68,4 +81,5 @@ export class WorkerManager {
       package: pkg 
     });
   }
+
 }
