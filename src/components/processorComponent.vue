80910694f8866d1cdd2a693329c98376dfa75f3b
JCMoure <juancarlos.moure@uab.es>
2026-01-11 13:04:48 +0100

    Processor Format

--- src/components/processorComponent.vue
+++ src/components/processorComponent.vue
@@ -139,7 +139,7 @@
   }
   .pipeline-img {
     width:           100%;
-    height:          300px;
+    height:          150px;
     display:         flex;
     align-items:     center;
     justify-content: center;
