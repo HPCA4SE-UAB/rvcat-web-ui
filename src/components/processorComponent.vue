c6a1c22b387b6551ca93331d6ce6b5b1937c9d27
JCMoure <juancarlos.moure@uab.es>
2026-01-11 07:45:10 +0100

    Update processorComponent.vue

--- src/components/processorComponent.vue
+++ src/components/processorComponent.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { ref, onMounted, onUnmounted, nextTick, inject, watch } from 'vue'
   import HelpComponent from '@/components/tutorialComponent.vue';
-  // import { useRvcatApi } from 'rvcat';
+  import { useRvcatApi } from '@/rvcatAPI';
 
   const { setProcessor, setROBSize, isReady } = useRVCAT_Api();
  
