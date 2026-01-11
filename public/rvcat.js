e3ea73a54fcb85e5ba4e1e6902fc74aa05a07a48
JCMoure <juancarlos.moure@uab.es>
2026-01-11 10:09:41 +0100

    Update rvcat.js

--- public/rvcat.js
+++ public/rvcat.js
@@ -1,6 +1,3 @@
-///////////////////////////////////////////////////////////////////////
-/////////// Functions calling PYTHON RVCAT  ///////////////////////////
-///////////////////////////////////////////////////////////////////////
 function readPythonProgramsAndProcessors() {
       programShow();
       getSchedulerAnalysis(1000,100);
@@ -11,14 +8,14 @@ function programShow() {
 }
 
 function insert_cache_annotations(cache) {
-    if(cache.nBlocks>0){
-      document.getElementById('cache-info').innerHTML=`
+  if (cache.nBlocks>0){
+     document.getElementById('cache-info').innerHTML=`
       <b>Cache:</b><span>${cache.nBlocks} blocks of ${cache.blkSize} bytes. Miss penalty: ${cache.mPenalty}. Miss Issue time: ${cache.mIssueTime}</span>`;
-    }
-    else {
-      document.getElementById('cache-info').innerHTML="<b>Processor does not simulate a cache memory.</b>";
-    }
   }
+  else {
+     document.getElementById('cache-info').innerHTML="<b>Processor does not simulate a cache memory.</b>";
+  }
+}
 
 function getProcessorGraph(processorInfo) {
   try {
