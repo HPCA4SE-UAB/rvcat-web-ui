5d97ac4d9b2099fa6f93a2eb72ddd747da6111c3
JCMoure <juancarlos.moure@uab.es>
2026-01-18 16:30:25 +0100

    Update procSettingsComponent.vue

--- src/components/procSettingsComponent.vue
+++ src/components/procSettingsComponent.vue
@@ -2,7 +2,7 @@
   import { ref, reactive, computed, onMounted, onUnmounted, nextTick} from "vue";
   import HelpComponent from '@/components/helpComponent.vue';
 
-  const simState            = inject('simulationState');
+  const simState = inject('simulationState');
 
   const dispatch        = ref(0);
   const retire          = ref(0);
