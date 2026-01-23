13cc5e57fda48aaf382c5919ffb8b8cbfc5e8fdd
JCMoure <juancarlos.moure@uab.es>
2026-01-23 17:52:26 +0100

    Update SimulatorView.vue

--- src/views/SimulatorView.vue
+++ src/views/SimulatorView.vue
@@ -21,31 +21,38 @@ const { importRVCAT }               = useRVCAT_Api();
 /* ------------------------------------------------------------------ 
  * Main Simulator Panel UI
  * ------------------------------------------------------------------ */
-  
+
 // full screen mode
 const FULL_NONE      = 0
 const FULL_PROCESSOR = 1
 const FULL_PROGRAM   = 2
 const FULL_TUTORIAL  = 3
 const fullComponent = ref(FULL_NONE);
-  
-const fullProcessorButtonText  = computed(() => 
-  (fullComponent.value == FULL_PROCESSOR)? '📍Edit Processor' : '📌Edit Processor'
-);
-const fullProgramButtonText  = computed(() => 
-  (fullComponent.value == FULL_PROGRAM)? '📍Edit Program' : '📌Edit Program'
-);
-const fullTutorialButtonText  = computed(() => 
-  (fullComponent.value == FULL_TUTORIAL)? '📍Edit Tutorial' : '📌Edit Tutorial'
-);
 
+// Computed para usar en template
+const isNotFullscreen       = computed(() => fullComponent.value === FULL_NONE);
+const isProcessorFullscreen = computed(() => fullComponent.value === FULL_PROCESSOR);
+const isProgramFullscreen   = computed(() => fullComponent.value === FULL_PROGRAM);
+const isTutorialFullscreen  = computed(() => fullComponent.value === FULL_TUTORIAL);
+
+const getButtonText = (componentType, baseText) => {
+  return computed(() => 
+    fullComponent.value === componentType ? `📍${baseText}` : `📌${baseText}`
+  );
+};
+
+// Button texts usando el helper
+const fullProcessorButtonText = getButtonText(FULL_PROCESSOR, 'Edit Processor');
+const fullProgramButtonText   = getButtonText(FULL_PROGRAM,   'Edit Program');
+const fullTutorialButtonText  = getButtonText(FULL_TUTORIAL,  'Edit Tutorial');
+
+// Container classes
 const containerClasses = computed(() => ({
-  'processor-fullscreen': fullComponent.value == FULL_PROCESSOR,
-  'program-fullscreen':   fullComponent.value == FULL_PROGRAM,
-  'tutorial-fullscreen':  fullComponent.value == FULL_TUTORIAL
-  // add more components here
+  'processor-fullscreen': isProcessorFullscreen.value,
+  'program-fullscreen':   isProgramFullscreen.value,
+  'tutorial-fullscreen':  isTutorialFullscreen.value
 }));
-
+  
 // Map of component keys -> component definitions
 const components = {
   timelineComponent,
@@ -137,22 +144,19 @@ function toggleFullscreen(component) {
         <ul>
           <li>
             <button class="blue-button" title="Open full window for processor configuration" 
-              :class="{ 'active': fullComponent.value == FULL_PROCESSOR }" 
-              @click="toggleFullscreen(FULL_PROCESSOR)" >
+              :class="{ 'active': isProcessorFullscreen }" @click="toggleFullscreen(FULL_PROCESSOR)" >
                 {{ fullProcessorButtonText }}
             </button>
           </li>
           <li>
             <button class="blue-button" title="Open full window for program edition" 
-              :class="{ 'active': fullComponent.value == FULL_PROGRAM}" 
-              @click="toggleFullscreen(FULL_PROGRAM)" >
+              :class="{ 'active': isProgramFullscreen }" @click="toggleFullscreen(FULL_PROGRAM)" >
                 {{ fullProgramButtonText }}
             </button>
           </li>
           <li>
             <button class="blue-button" title="Open full window for tutorial edition" 
-              :class="{ 'active': fullComponent.value == FULL_TUTORIAL}" 
-              @click="toggleFullscreen(FULL_TUTORIAL)" >
+              :class="{ 'active': isTutorialFullscreen }" @click="toggleFullscreen(FULL_TUTORIAL)" >
                 {{ fullTutorialButtonText }}
             </button>
           </li>
@@ -198,19 +202,19 @@ function toggleFullscreen(component) {
     
     <main class="container" :class="containerClasses">
       
-      <div v-show="fullComponent === FULL_PROCESSOR" class="grid-item processor" :class="{ 'fullscreen': fullComponent === FULL_PROCESSOR }">
-        <processorComponent :is-fullscreen="fullComponent === FULL_PROCESSOR" />
+      <div v-show="isProcessorFullscreen" class="grid-item processor" :class="{ 'fullscreen': isProcessorFullscreen }">
+        <processorComponent :is-fullscreen="isProcessorFullscreen" />
       </div>
       
-      <div v-show="fullComponent === FULL_PROGRAM" class="grid-item program" :class="{ 'fullscreen': fullComponent === FULL_PROGRAM }">
-        <programComponent :is-fullscreen="fullComponent === FULL_PROGRAM" />
+      <div v-show="isProgramFullscreen" class="grid-item program" :class="{ 'fullscreen': isProgramFullscreen }">
+        <programComponent :is-fullscreen="isProgramFullscreen" />
       </div>
 
-      <div v-show="fullComponent === FULL_TUTORIAL" class="grid-item tutorial" :class="{ 'fullscreen': fullComponent === FULL_TUTORIAL }">
-        <tutorialEditor :is-fullscreen="fullComponent === FULL_TUTORIAL" />
+      <div v-show="isTutorialFullscreen" class="grid-item tutorial" :class="{ 'fullscreen': isTutorialFullscreen }">
+        <tutorialEditor :is-fullscreen="isTutorialFullscreen" />
       </div>
       
-      <div v-show="!isFullscreen" class="grid-item results">
+      <div v-show= "isNotFullscreen" class="grid-item results">
         <component :is="currentComponent" v-if="currentComponent" ref="settingsCompInst" />
         <div v-else>Component not found</div>
       </div>
@@ -283,10 +287,12 @@ nav ul li {
   
 .processor { grid-column: 1; grid-row: 1; }
 .program   { grid-column: 1; grid-row: 2; }
+.tutorial  { grid-column: 1; grid-row: 2; }
 .results   { grid-column: 2; grid-row: 1 / 3; min-width: 0;}
 
 .container.processor-fullscreen, 
-.container.program-fullscreen {
+.container.program-fullscreen,
+.container.tutorial-fullscreen {
   grid-template-columns: 1fr;
   grid-template-rows:    1fr;
   position: fixed; 
@@ -311,12 +317,14 @@ nav ul li {
   overflow: hidden;
 }
 .grid-item.program,
-.grid-item.results {
+.grid-item.results,
+.grid-item.tutorial {
   display: grid;
 }
 
 .grid-item.processor.fullscreen,
-.grid-item.program.fullscreen {
+.grid-item.program.fullscreen,
+.grid-item.tutorial.fullscreen {
   grid-column:   1 / span 3;
   grid-row:      1;
   height:        100%;
@@ -334,9 +342,14 @@ nav ul li {
     
 /* Ocultar otros componentes en pantalla completa */
 .container.processor-fullscreen .grid-item.program,
+.container.processor-fullscreen .grid-item.tutorial,
 .container.processor-fullscreen .grid-item.results,
 .container.program-fullscreen .grid-item.processor,
-.container.program-fullscreen .grid-item.results {
+.container.program-fullscreen .grid-item.tutorial,
+.container.program-fullscreen .grid-item.results,
+.container.tutorial-fullscreen .grid-item.processor,
+.container.tutorial-fullscreen .grid-item.program,
+.container.tutorial-fullscreen .grid-item.results {
   display: none;
 }
   
@@ -379,25 +392,27 @@ nav ul li {
   to { transform: rotate(360deg); }
 }
 
-.grid-item.processor.fullscreen .processor-content,
-.grid-item.program.fullscreen .processor-content{
+.grid-item.processor.fullscreen .processor-content {
   padding:   0px;
   max-width: 1400px;
   margin:    0 auto;
 }
 
 .grid-item.processor.fullscreen::-webkit-scrollbar,
-.grid-item.program.fullscreen::-webkit-scrollbar {
+.grid-item.program.fullscreen::-webkit-scrollbar,
+.grid-item.tutorial.fullscreen::-webkit-scrollbar {
   width: 10px;
 }
 
 .grid-item.processor.fullscreen::-webkit-scrollbar-track,
-.grid-item.program.fullscreen::-webkit-scrollbar-track {
+.grid-item.program.fullscreen::-webkit-scrollbar-track,
+.grid-item.tutorial.fullscreen::-webkit-scrollbar-track {
   background: #f1f1f1;
 }
 
 .grid-item.processor.fullscreen::-webkit-scrollbar-thumb,
-.grid-item.program.fullscreen::-webkit-scrollbar-thumb {
+.grid-item.program.fullscreen::-webkit-scrollbar-thumb,
+.grid-item.tutorial.fullscreen::-webkit-scrollbar-thumb {
   background:    #888;
   border-radius: 5px;
 }
