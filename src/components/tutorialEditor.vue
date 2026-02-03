92896e4db424c840b1fb8514f8afcf0a59a81f13
JCMoure <juancarlos.moure@uab.es>
2026-02-03 14:46:15 +0100

    Update tutorialEditor.vue

--- src/components/tutorialEditor.vue
+++ src/components/tutorialEditor.vue
@@ -43,7 +43,7 @@
             <div class="label-input-row">
               <label>Tutorial ID <span class="required">*</span></label>
               <input v-model="tutorial.id" type="text" :placeholder="tutorial.id? tutorial.id+'*' : 'filename'" class="name-input">
-	    </div>
+	          </div>
             <div class="label-input-row">
               <label>Tutorial Name <span class="required">*</span></label>
               <input v-model="tutorial.name" type="text" placeholder="Enter tutorial name" class="name-input">
@@ -60,7 +60,7 @@
 
         <!-- Card: step/question -->
         <div class="section">
-          <div v-for="(step, index) in tutorial.steps" :key="index" class="step-card" :class="{ 'question-card': step.type === 'question' }">
+          <div :key="tutorialOptions.selectedStep" :step="tutorial.steps[key]" class="step-card" :class="{ 'question-card': step.type === 'question' }">
             <div class="form-group left-column">
               <span class="step-number" :class="{ 'question-number': step.type === 'question' }">{{ index + 1 }}</span>
               <label>{{step.type === 'question' ? 'Question title' : 'Step title'}}<span class="required">*</span></label>
