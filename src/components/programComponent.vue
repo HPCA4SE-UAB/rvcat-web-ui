66c59054bb67578305d983c5a8ba47c22a554a85
JCMoure <juancarlos.moure@uab.es>
2026-02-04 19:07:42 +0100

    Update programComponent.vue

--- src/components/programComponent.vue
+++ src/components/programComponent.vue
@@ -786,10 +786,11 @@ function cancelPendingAction() {
         </select>
       </div>
 
-      <section class="main-box code-block"> <pre><code>{{ programText }}</code></pre> </section>
     </div>
     
-    <div v-else class="header fullscreen-header">
+    <section v-if="!isFullscreen" class="main-box code-block"> <pre><code>{{ programText }}</code></pre> </section>
+    
+    <div v-if="isFullscreen" class="header fullscreen-header">
       <div class="section-title-and-info">
         <span ref="helpIcon" class="info-icon" @click="openHelp" title="Show help"><img src="/img/info.png" class="info-img"></span>
         <span class="header-title">Program - <strong>{{ programOptions.currentProgram }}</strong></span>
