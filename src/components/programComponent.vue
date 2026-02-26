3a6497476b6453974e1ec8a90e7236d16e6de561
JCMoure <juancarlos.moure@uab.es>
2026-02-26 18:41:04 +0100

    Do not call RVCAT

--- src/components/programComponent.vue
+++ src/components/programComponent.vue
@@ -9,7 +9,7 @@
   const { getProgGraph, setProgram, showProgram } = useRVCAT_Api();
   const { registerHandler }                       = inject('worker');
   const simState                                  = inject('simulationState');
-  
+
   const props = defineProps({
     isFullscreen: {
       type:    Boolean,
@@ -60,7 +60,7 @@ const STORAGE_KEY = 'programOptions'
   const programSvg     = ref('')
   const showFullScreen = ref(false);
   let   graphTimeout   = null
- 
+
   const saveOptions = () => {
     try {
       localStorage.setItem(STORAGE_KEY, JSON.stringify(programOptions))
@@ -71,7 +71,7 @@ const STORAGE_KEY = 'programOptions'
 
 
 // ============================================================================
-// Temporal in-edition program 
+// Temporal in-edition program
 // ============================================================================
 
 const editedProgram = ref([]);
@@ -83,7 +83,7 @@ function loadEditedProgram() {
     const data = JSON.parse(stored);
     editedProgram.value = (data.instruction_list || []).map(inst => {
       return {
-        text:    inst.text || '',    type:    inst.type    || '',  oper:     inst.oper    || '',  
+        text:    inst.text || '',    type:    inst.type    || '',  oper:     inst.oper    || '',
         size:    inst.size || '',    destin:  inst.destin  || '',  source1:  inst.source1 || '',
         source2: inst.source2 || '', source3: inst.source3 || '',  constant: inst.constant || ''
       };
@@ -133,30 +133,30 @@ function loadEditedProgram() {
     if (newValue == 2)   // This is an initialization step
        initProgram()  // --> generates reloadProgram
   });
-  
+
   // Watch for changes on processor configuration
   watch(() => simState.selectedProcessor, (newValue, oldValue) => {
     if (simState.state > 2 && simState.selectedProgram != '') {
       console.log('📄🔄 Refreshing program visualization...');
-      showProgram()
+      // showProgram()
     }
   });
-  
+
    // Handler for 'set_program' message (fired by this component)
   const handleSetProgram = async (data, dataType) => {
     if (dataType === 'error') {
       console.error('📄❌ Failed to set program:', data);
       return;
     }
-    try {    
+    try {
       simState.selectedProgram = programOptions.currentProgram;  // fire other components, watching for a change
       if (simState.state == 2) {  // This is an initialization step
         simState.state = 3;       // Change to next initialization step
         console.log('📄✅ Initialization step (3): program provided to RVCAT')
-      } 
+      }
       if (simState.selectedProgram != '')
-        showProgram()  // obtain text from RVCAT API (id= 'show_program')
-      
+        // showProgram()  // obtain text from RVCAT API (id= 'show_program')
+
     } catch (error) {
       console.error('📄❌ Failed to set program:', error)
     }
@@ -205,10 +205,10 @@ function loadEditedProgram() {
 
 
 // ============================================================================
-// PROGRAM ACTIONS: InitProgram, reloadProgram, editProgram, removeProgram, 
+// PROGRAM ACTIONS: InitProgram, reloadProgram, editProgram, removeProgram,
 //               uploadForEdition
 // ============================================================================
-  
+
   const initProgram = async () => {
     const savedProgram = programOptions.currentProgram
     await initResource('program', programOptions, 'currentProgram', 'availablePrograms')
@@ -221,13 +221,23 @@ function loadEditedProgram() {
     try {
       const jsonString      = localStorage.getItem(`program.${programOptions.currentProgram}`)
       uploadedProgramObject = JSON.parse(jsonString)
-      setProgram( jsonString ) // Call Python RVCAT to load new program --> id= 'set-program'
+      // setProgram( jsonString ) // Call Python RVCAT to load new program --> id= 'set-program'
+
+      simState.selectedProgram = programOptions.currentProgram;  // fire other components, watching for a change
+      if (simState.state == 2) {  // This is an initialization step
+        simState.state = 3;       // Change to next initialization step
+        console.log('📄✅ Initialization step (3): program provided to RVCAT')
+      }
+      if (simState.selectedProgram != '')
+        // showProgram()  // obtain text from RVCAT API (id= 'show_program')
+
+
     } catch (error) {
       console.error('📄❌ Failed to set program:', error)
-    }      
+    }
   }
 
-  const emit = defineEmits(['requestSwitchFull']) 
+  const emit = defineEmits(['requestSwitchFull'])
 
   function editProgram () {
     if (uploadedProgramObject) {
@@ -253,7 +263,7 @@ function loadEditedProgram() {
   }
 
 // UpLOAD from Edition Panel: straightforward version (no modal)
-const uploadForEdition = async () => { 
+const uploadForEdition = async () => {
   try {
     const data = await uploadJSON(null, 'program');
     if (data) {
@@ -270,13 +280,13 @@ const uploadForEdition = async () => {
 };
 
 // ============================================================================
-// Program handling: 
-//        addInstruction,       removeInstruction, 
+// Program handling:
+//        addInstruction,       removeInstruction,
 //        moveInstructionUp,    moveInstructionDown,
 //        normalizeInstruction, snapshotProgram
 // ============================================================================
 
-// Add just at index position 
+// Add just at index position
 function addInstruction( index ) {
   editedProgram.value.splice( index, 0, {
       text:    '',
@@ -341,7 +351,7 @@ function snapshotProgram() {
 // viewColumns
 // ============================================================================
 
-  function toggleInOut  () { 
+  function toggleInOut  () {
     programOptions.showInOut           = !programOptions.showInOut
     programOptions.visibleCols.destin  = !programOptions.visibleCols.destin
     programOptions.visibleCols.source1 = !programOptions.visibleCols.source1
@@ -350,9 +360,9 @@ function snapshotProgram() {
     programOptions.visibleCols.constant= !programOptions.visibleCols.constant
   }
 
-  function toggleActions() { 
+  function toggleActions() {
     programOptions.showActions         = !programOptions.showActions
-    programOptions.visibleCols.actions = !programOptions.visibleCols.actions   
+    programOptions.visibleCols.actions = !programOptions.visibleCols.actions
   }
 
   // Handler for 'get_prog_graph' message (fired by RVCAT getProgGraph function)
@@ -362,7 +372,7 @@ function snapshotProgram() {
       return;
     }
     try {
-       const svg        = await createGraphVizGraph(data);  
+       const svg        = await createGraphVizGraph(data);
        programSvg.value = svg.outerHTML;
        console.log('📄✅ generate dependence graph of edited program')
     } catch (error) {
@@ -411,8 +421,8 @@ function snapshotProgram() {
     }
     showModalDownload.value = false;
   }
- 
-  const uploadProgram = async (oldProgram) => {  
+
+  const uploadProgram = async (oldProgram) => {
     try {
       const data = await uploadJSON(null, 'program');
       if (data) {
@@ -422,13 +432,13 @@ function snapshotProgram() {
         else {
           // TODO: Check here if it is a valid program
           uploadedProgramObject = data
-          saveToLocalStorage('program', data.name, uploadedProgramObject, 
+          saveToLocalStorage('program', data.name, uploadedProgramObject,
                                         programOptions.availablePrograms)
           programOptions.currentProgram = data.name;
           return;
         }
       }
-      programOptions.currentProgram = oldProgram;       
+      programOptions.currentProgram = oldProgram;
     } catch (error) {
       programOptions.currentProgram = oldProgram;
     }
@@ -440,10 +450,10 @@ function snapshotProgram() {
     showModalClear.value = false;
   }
 
-  
+
 
 // ============================================================================
-// Help support 
+// Help support
 // ============================================================================
   const showHelp     = ref(false);
   const helpPosition = ref({ top: '0%', left: '40%' });
@@ -456,15 +466,15 @@ function snapshotProgram() {
 
 <template>
   <div class="main">
-    
+
     <div v-if="!isFullscreen" class="header">
       <div class="section-title-and-info">
         <span ref="helpIcon" class="info-icon" @click="openHelp" title="Show help"><img src="/img/info.png" class="info-img"></span>
         <span class="header-title">Program</span>
       </div>
-      
+
       <div class="settings-container">
-        <select v-model="programOptions.currentProgram" class="form-select" 
+        <select v-model="programOptions.currentProgram" class="form-select"
                id="programs-list" title="Select Program">
           <option value="" disabled>Select</option>
           <option v-for="program in programOptions.availablePrograms" :key="program" :value="program">
@@ -472,8 +482,8 @@ function snapshotProgram() {
           </option>
            <option value="_add_new_">Add new</option>
         </select>
-        <button class="blue-button small-btn" @click="editProgram" 
-            id="edit-program-button" 
+        <button class="blue-button small-btn" @click="editProgram"
+            id="edit-program-button"
             title="Edit current program on full-screen as a new program">
           📝
         </button>
@@ -484,9 +494,9 @@ function snapshotProgram() {
         </button>
       </div>
     </div>
-    
+
     <section v-if="!isFullscreen" class="main-box code-block"> <pre><code>{{ programText }}</code></pre> </section>
-    
+
     <div v-if="isFullscreen" class="header">
       <div class="section-title-and-info">
         <span ref="helpIcon" class="info-icon" @click="openHelp" title="Show help">
@@ -495,49 +505,49 @@ function snapshotProgram() {
         <span class="header-title">Program Editor</span>
       </div>
 
-      <button class="blue-button add-prev-margin" :class="{ active: programOptions.showInOut }"  
+      <button class="blue-button add-prev-margin" :class="{ active: programOptions.showInOut }"
           title="Show/Hide instruction Input/Output operands"
           id="show-inout-operands"
-        @click="toggleInOut"> 
+        @click="toggleInOut">
         <span v-if="programOptions.showInOut">✔ </span>
         InOut
       </button>
 
-      <button class="blue-button add-margin" :class="{ active: programOptions.showActions }"  
+      <button class="blue-button add-margin" :class="{ active: programOptions.showActions }"
           title="Show/Hide column with buttons for moving/adding/removing instructions"
           id="show-instruction-actions"
-         @click="toggleActions"> 
+         @click="toggleActions">
         <span v-if="programOptions.showActions">✔ </span>
         Actions
       </button>
 
-      <button class="icon-button add-prev-margin" @click="openFullScreen" 
+      <button class="icon-button add-prev-margin" @click="openFullScreen"
          title="View program's dependence graph on full-screen"
          id="open-full-dependence-graph"
         >
          <img src="/img/fullscreen.png" class="btn-img">
       </button>
-     
+
       <div class="settings-container fullscreen-settings">
         <div class="buttons">
 
           <button class="blue-button" @click="showModalDownload = true"
-               title="Save current edited program" 
-               id="program-download-button"> 
-            Download 
+               title="Save current edited program"
+               id="program-download-button">
+            Download
           </button>
 
           <button class="blue-button" @click="uploadForEdition"
-               title="Load new program from file system for edition"     
-               id="program-upload-button">  
-            Upload  
+               title="Load new program from file system for edition"
+               id="program-upload-button">
+            Upload
           </button>
 
         </div>
         <div class="buttons">
 
           <button class="blue-button  add-margin" @click="showModalClear = true"
-                title="Clear edition and start new program from scratch" 
+                title="Clear edition and start new program from scratch"
                 id="clear-program-button">
             Clear
           </button>
@@ -591,8 +601,8 @@ function snapshotProgram() {
                       class="table-select" title="Select operation for this type"
                     >
                       <option value="">Select...</option>
-                      <option 
-                        v-for="operation in typeOperations[inst.type]" 
+                      <option
+                        v-for="operation in typeOperations[inst.type]"
                         :key="operation"
                         :value="operation"
                       >
@@ -606,9 +616,9 @@ function snapshotProgram() {
                       class="table-select" title="Select size for this instruction type & operations"
                     >
                       <option value="">Select...</option>
-                      <option 
-                        v-for="size in typeSizes[inst.type]" 
-                        :key="size" 
+                      <option
+                        v-for="size in typeSizes[inst.type]"
+                        :key="size"
                         :value="size"
                       >
                         {{ size }}
@@ -638,8 +648,8 @@ function snapshotProgram() {
 
                   <td v-if="programOptions.visibleCols.actions" class="actions-cell">
 
-                    <button 
-                      @click="moveInstructionUp(index)" 
+                    <button
+                      @click="moveInstructionUp(index)"
                       :disabled="index === 0"
                       class="action-btn"
                       title="Move up"
@@ -647,8 +657,8 @@ function snapshotProgram() {
                       ▲
                     </button>
 
-                    <button 
-                      @click="moveInstructionDown(index)" 
+                    <button
+                      @click="moveInstructionDown(index)"
                       :disabled="index === editedProgram.length - 1"
                       class="action-btn"
                       title="Move down"
@@ -656,8 +666,8 @@ function snapshotProgram() {
                       ▼
                     </button>
 
-                    <button 
-                      @click="removeInstruction(index)" 
+                    <button
+                      @click="removeInstruction(index)"
                       :disabled="editedProgram.length === 1"
                       class="action-btn delete"
                       title="Delete"
@@ -665,8 +675,8 @@ function snapshotProgram() {
                       ✕
                     </button>
 
-                    <button 
-                      @click="addInstruction(index)" 
+                    <button
+                      @click="addInstruction(index)"
                       :disabled="editedProgram.length === 1"
                       class="action-btn"
                       title="Insert new instruction before this one"
@@ -768,12 +778,12 @@ function snapshotProgram() {
       </div>
     </div>
   </div>
-  
+
   <div v-if="showModalDownload" class="modal-overlay">
     <div class="modal">
       <h4>Save Program As</h4>
       <label for="config-name">Name:</label>
-      <input id="save-program-name" type="text" v-model="modalName"  
+      <input id="save-program-name" type="text" v-model="modalName"
           title="file name of new program" />
       <div v-if="nameError" class="error">{{ nameError }}</div>
       <div class="modal-actions">
@@ -795,9 +805,9 @@ function snapshotProgram() {
 
   <Teleport to="body">
     <HelpComponent v-if="showHelp" :position="helpPosition"
-    text="The simulated program consists of a <em>fixed-iteration</em> loop executing a sequence of <strong>machine instructions</strong>, each described in a high-level, 
+    text="The simulated program consists of a <em>fixed-iteration</em> loop executing a sequence of <strong>machine instructions</strong>, each described in a high-level,
        informal language. The <em>type</em>, execution <em>latency</em> and eligible <em>execution ports</em> are shown for each instruction.
-        <p>The simulation tracks <strong>data dependencies</strong> but omits detailed architectural state: it <strong>does not</strong> model processor registers, memory states, 
+        <p>The simulation tracks <strong>data dependencies</strong> but omits detailed architectural state: it <strong>does not</strong> model processor registers, memory states,
       branch outcomes, or memory dependencies (e.g., store-load interactions).</p>
         Programs can be uploaded or downloaded in JSON format."
       title="Program Loop"
@@ -815,7 +825,7 @@ function snapshotProgram() {
   font-size:     2.2vh;
   border-radius: 10px;
 }
-  
+
 ::-webkit-scrollbar {
   width: 5px;
 }
@@ -834,7 +844,7 @@ function snapshotProgram() {
   flex-direction: column;
   overflow:       hidden;
 }
-  
+
 .settings-container {
   display:     flex;
   align-items: center;
@@ -1063,7 +1073,7 @@ function snapshotProgram() {
   background:    #ffebee;
   border-radius: 4px;
 }
-  
+
 .form-select {
   width:            100%;
   padding:          1px 1px;
@@ -1085,7 +1095,7 @@ function snapshotProgram() {
   font-weight:      bold;
   background-color: #f0f5ff;
 }
-  
+
   .instruction-side-container {
     display:        flex;
     min-width:      0;
@@ -1101,8 +1111,8 @@ function snapshotProgram() {
 
   .memory-side-container {
     flex:       1 1 30%;
-    min-width:  0;           
-    box-sizing: border-box; 
+    min-width:  0;
+    box-sizing: border-box;
     display:    flex;
     padding:    3px;
     flex-direction: column;
@@ -1113,7 +1123,7 @@ function snapshotProgram() {
 
   .instruction-side-container .table-container,
   .memory-side-container      .table-container {
-    flex: 0 0 auto; 
+    flex: 0 0 auto;
   }
 
   .fullscreen-overlay {
@@ -1145,7 +1155,7 @@ function snapshotProgram() {
     flex-direction: column;
     box-shadow:     0 4px 12px rgba(0,0,0,0.25);
   }
- 
+
   .fullscreen-content .close-btn {
     align-self: flex-end;
     background: none;
@@ -1154,7 +1164,7 @@ function snapshotProgram() {
     cursor:     pointer;
     margin-bottom: 8px;
   }
-  
+
   .fullscreen-header {
     display:         flex;
     justify-content: space-between;
@@ -1219,7 +1229,7 @@ function snapshotProgram() {
     height: 1.2em;
   }
   .icon-button:hover {
-    background: #a0a0a0;      /* darker at hover */  
+    background: #a0a0a0;      /* darker at hover */
   }
   .icon-button:active {
     background: #909090;      /* still darker */
